191473636d8233777b0db2287434e012ded0468a
Jonas Lukasczyk <jl@jluk.de>
2023-08-29 16:13:20 +0200

    bright theme

--- pycinema/theater/node_editor/NodeEditorStyle.py
+++ pycinema/theater/node_editor/NodeEditorStyle.py
@@ -1,45 +1,75 @@
-from PySide6 import QtGui
+from PySide6 import QtGui, QtCore
 
-Z_EDGE_LAYER = 400
-Z_NODE_LAYER = 500
+class NodeEditorStyle:
 
-NODE_HEADER_HEIGHT = 20
-NODE_BORDER_WIDTH = 5
-# NODE_BORDER_RADIUS = 5
-NODE_SEP_WIDTH = 5
-NODE_WIDTH = 200
-NODE_MARGIN = 3
-NODE_SEP_Y = 25
-NODE_PORT_Y = 35
-NODE_PORT_SPACE = 25
+  Z_EDGE_LAYER = 400
+  Z_NODE_LAYER = 500
 
-PORT_SIZE = 8
-PORT_BORDER_WIDTH = 2
-# bright
-# COLOR_BASE = QtGui.QColor('#d5d5d5')
-# COLOR_NORMAL_ = '#000'
+  NODE_HEADER_HEIGHT = 20
+  NODE_BORDER_WIDTH = 5
+  NODE_SEP_WIDTH = 5
+  NODE_WIDTH = 200
+  NODE_MARGIN = 3
+  NODE_SEP_Y = 25
+  NODE_PORT_Y = 35
+  NODE_PORT_SPACE = 25
 
-# dark
-# COLOR_BASE = QtGui.QColor('#27292b')
-COLOR_BASE = QtGui.QColor('#999')
-COLOR_BASE_T = QtGui.QColor('#666')
-COLOR_BASE_T.setAlpha(200)
-COLOR_NORMAL_ = '#ddd'
-COLOR_DISABLED_ = '#999'
+  PORT_SIZE = 8
+  PORT_BORDER_WIDTH = 2
 
-COLOR_BACKGROUND = QtGui.QColor('#333438')
-# COLOR_BACKGROUND = QtGui.QColor('#f00')
-# COLOR_BASE = QtGui.QColor('#444')
-# COLOR_BORDER = QtGui.QColor('#27292b')
-COLOR_BORDER = QtGui.QColor('#ff0000')
-COLOR_WIDGET = QtGui.QColor('#505050')
+  COLOR_BASE = None
+  COLOR_BASE_T = None
+  COLOR_NORMAL_ = None
+  COLOR_DISABLED_ = None
+  COLOR_BACKGROUND = None
+  COLOR_BORDER = None
+  COLOR_WIDGET = None
+  COLOR_WIDGET_T = None
+  COLOR_RED = None
+  COLOR_RED_T = None
+  COLOR_BLUE = None
+  COLOR_BLUE_T = None
+  COLOR_NORMAL = None
 
-COLOR_RED = QtGui.QColor('#98281b')
-COLOR_RED_T = QtGui.QColor('#98281b')
-COLOR_RED_T.setAlpha(200)
+  def update_theme():
 
-COLOR_BLUE = QtGui.QColor('#15a3b4')
-COLOR_BLUE_T = QtGui.QColor('#15a3b4')
-COLOR_BLUE_T.setAlpha(200)
+    base_color = QtCore.QCoreApplication.instance().palette().base().color()
 
-COLOR_NORMAL = QtGui.QColor(COLOR_NORMAL_)
+    if base_color.red()<100:
+      #dark theme
+      NodeEditorStyle.COLOR_BASE = QtGui.QColor('#999')
+      NodeEditorStyle.COLOR_BASE_T = QtGui.QColor('#666')
+      NodeEditorStyle.COLOR_BASE_T.setAlpha(200)
+      NodeEditorStyle.COLOR_NORMAL_ = '#ddd'
+      NodeEditorStyle.COLOR_DISABLED_ = '#999'
+      NodeEditorStyle.COLOR_BACKGROUND = QtGui.QColor('#333438')
+      NodeEditorStyle.COLOR_BORDER = QtGui.QColor('#ff0000')
+      NodeEditorStyle.COLOR_WIDGET = QtGui.QColor('#505050')
+      NodeEditorStyle.COLOR_WIDGET_T = QtGui.QColor('#444')
+      NodeEditorStyle.COLOR_WIDGET_T.setAlpha(200)
+      NodeEditorStyle.COLOR_RED = QtGui.QColor('#98281b')
+      NodeEditorStyle.COLOR_RED_T = QtGui.QColor('#98281b')
+      NodeEditorStyle.COLOR_RED_T.setAlpha(200)
+      NodeEditorStyle.COLOR_BLUE = QtGui.QColor('#15a3b4')
+      NodeEditorStyle.COLOR_BLUE_T = QtGui.QColor('#15a3b4')
+      NodeEditorStyle.COLOR_BLUE_T.setAlpha(200)
+      NodeEditorStyle.COLOR_NORMAL = QtGui.QColor(NodeEditorStyle.COLOR_NORMAL_)
+    else:
+      #bright theme
+      NodeEditorStyle.COLOR_BASE = QtGui.QColor('#bbb')
+      NodeEditorStyle.COLOR_BASE_T = QtGui.QColor('#ccc')
+      NodeEditorStyle.COLOR_BASE_T.setAlpha(200)
+      NodeEditorStyle.COLOR_NORMAL_ = '#000'
+      NodeEditorStyle.COLOR_DISABLED_ = '#aaa'
+      NodeEditorStyle.COLOR_BACKGROUND = QtGui.QColor('#aaa')
+      NodeEditorStyle.COLOR_BORDER = QtGui.QColor('#ff0000')
+      NodeEditorStyle.COLOR_WIDGET = QtGui.QColor('#eee')
+      NodeEditorStyle.COLOR_WIDGET_T = QtGui.QColor('#eee')
+      NodeEditorStyle.COLOR_WIDGET_T.setAlpha(200)
+      NodeEditorStyle.COLOR_RED = QtGui.QColor('#f24940')
+      NodeEditorStyle.COLOR_RED_T = QtGui.QColor('#f24940')
+      NodeEditorStyle.COLOR_RED_T.setAlpha(200)
+      NodeEditorStyle.COLOR_BLUE = QtGui.QColor('#96deec')
+      NodeEditorStyle.COLOR_BLUE_T = QtGui.QColor('#96deec')
+      NodeEditorStyle.COLOR_BLUE_T.setAlpha(200)
+      NodeEditorStyle.COLOR_NORMAL = QtGui.QColor(NodeEditorStyle.COLOR_NORMAL_)
