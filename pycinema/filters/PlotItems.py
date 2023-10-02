1729b47e9d20c9ba425bca5ba34ac0032c42498d
David H. Rogers <dhr@lanl.gov>
2023-10-02 11:30:17 -0600

    updating plot/plotitems

--- pycinema/filters/PlotItems.py
+++ pycinema/filters/PlotItems.py
@@ -3,6 +3,13 @@ from pycinema import Filter
 import PIL
 import numpy
 
+#
+# PlotItems
+#
+# To be paired with a plot view
+# Question: should this be a filter, or some new thing?
+# Doesn't seem to fit the design of a view or filter
+#
 class PlotItems(Filter):
 
     def __init__(self):
