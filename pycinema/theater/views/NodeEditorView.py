922f27f04a6a9c59b30b753d23d4ca947de56f60
David H. Rogers <dhr@lanl.gov>
2024-01-16 08:03:08 -0700

    fixed spelling error

--- pycinema/theater/views/NodeEditorView.py
+++ pycinema/theater/views/NodeEditorView.py
@@ -47,10 +47,10 @@ class QtNodeEditorView(QtWidgets.QGraphicsView):
         self.setSceneRect(-l,-l,2*l,2*l)
 
         if not QtNodeEditorView.scene:
-          QtNodeEditorView.init_gobal()
+          QtNodeEditorView.init_global()
         self.setScene(QtNodeEditorView.scene)
 
-    def init_gobal():
+    def init_global():
         node_connection_line = QtWidgets.QGraphicsLineItem()
         node_connection_line.setPen(QtGui.QPen(NES.COLOR_NORMAL, 2, QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
         node_connection_line.setZValue(1000)
