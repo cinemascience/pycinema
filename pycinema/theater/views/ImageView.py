0b7e138c6f83b3a160561a730b1478c6fa89198b
David H. Rogers <dhr@lanl.gov>
2024-01-17 16:01:52 -0700

    adding focus border; abstracting imageview layout

--- pycinema/theater/views/ImageView.py
+++ pycinema/theater/views/ImageView.py
@@ -4,6 +4,41 @@ from pycinema.theater.views.FilterView import FilterView
 import numpy
 from PySide6 import QtCore, QtWidgets, QtGui
 
+#
+# a class to manage the layout of images in an ImageView 
+class _ImageLayout():
+
+    def __init__(self):
+        self.view = None
+
+    def addImages(self, view, images):
+        max_w = 0
+        max_h = 0
+        if not type(images) is list:
+          return 1
+
+        for image in images:
+          max_w = max(image.shape[1],max_w)
+          max_h = max(image.shape[0],max_h)
+
+        max_w += 5
+        max_h += 5
+
+        n = max(len(images),1)
+        nRows = int(numpy.floor(numpy.sqrt(n)))
+        nCols = int(numpy.ceil(n/nRows))
+
+        total_h = 4*nRows*max_h
+        total_w = 4*nCols*max_w
+        view.setSceneRect(-total_w,-total_h,2*total_w,2*total_h)
+
+        for i, image in enumerate(images):
+          r = numpy.floor(i/nCols)
+          c = i-r*nCols
+          view.addImage(image, c*max_w, r*max_h)
+
+        view.fitInView()
+
 class _ImageViewer(QtWidgets.QGraphicsView):
 
     def __init__(self):
@@ -21,6 +56,9 @@ class _ImageViewer(QtWidgets.QGraphicsView):
         self.setScene(self._scene)
 
         self.items = []
+
+        # initialize the view focus border
+        self.setStyleSheet("border:3px solid transparent")
 
     def removeImages(self):
         for item in self.items:
@@ -74,6 +112,14 @@ class _ImageViewer(QtWidgets.QGraphicsView):
         super().resizeEvent(event)
         self.fitInView()
 
+    def focusInEvent(self,event):
+        # turn on view focus border
+        self.setStyleSheet("border:3px solid #15a3b4")
+
+    def focusOutEvent(self,event):
+        # turn off view focus border
+        self.setStyleSheet("border:3px solid transparent")
+
 class ImageView(Filter, FilterView):
 
     def __init__(self):
@@ -90,38 +136,14 @@ class ImageView(Filter, FilterView):
           }
         )
 
+        self.layout = _ImageLayout()
+
     def generateWidgets(self):
         self.view = _ImageViewer()
         self.content.layout().addWidget(self.view,1)
 
     def _update(self):
         self.view.removeImages()
-
-        max_w = 0
-        max_h = 0
-        images = self.inputs.images.get()
-        if not type(images) is list:
-          return 1
-
-        for image in images:
-          max_w = max(image.shape[1],max_w)
-          max_h = max(image.shape[0],max_h)
-        max_w += 5
-        max_h += 5
-
-        n = max(len(images),1)
-        nRows = int(numpy.floor(numpy.sqrt(n)))
-        nCols = int(numpy.ceil(n/nRows))
-
-        total_h = 4*nRows*max_h
-        total_w = 4*nCols*max_w
-        self.view.setSceneRect(-total_w,-total_h,2*total_w,2*total_h)
-
-        for i, image in enumerate(images):
-          r = numpy.floor(i/nCols)
-          c = i-r*nCols
-          self.view.addImage(image,c*max_w,r*max_h)
-
-        self.view.fitInView()
+        self.layout.addImages( self.view, self.inputs.images.get() )
 
         return 1
