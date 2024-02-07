f284f7aef8132a50bb250f6e817c8eebfc785380
David H. Rogers <dhr@lanl.gov>
2024-02-07 15:43:42 -0700

    testing/ScriptTesting.py

--- testing/ScriptTesting.py
+++ testing/ScriptTesting.py
@@ -2,6 +2,6 @@ import filecmp
 
 def test_scripttesting():
     # compare files that are written by an outside script
-    assert filecmp.cmp('log.txt', 'testing/gold/scripttest/log.txt')
-    assert filecmp.cmp('browse.txt', 'testing/gold/scripttest/browse.txt')
-    assert filecmp.cmp('query.txt', 'testing/gold/scripttest/query.txt')
+    assert filecmp.cmp('log.txt', 'testing/gold/scripttesting/log.txt')
+    assert filecmp.cmp('browse.txt', 'testing/gold/scripttesting/browse.txt')
+    assert filecmp.cmp('query.txt', 'testing/gold/scripttesting/query.txt')
