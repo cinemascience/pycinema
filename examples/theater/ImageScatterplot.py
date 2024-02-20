e2dcbdb9eb1fbff666440c528af360f4a5a8d8c8
David H. Rogers <dhr@lanl.gov>
2024-02-20 16:44:21 -0700

    updating examples

--- examples/theater/ImageScatterplot.py
+++ examples/theater/ImageScatterplot.py
@@ -6,22 +6,12 @@ import pycinema.theater.views
 # pycinema settings
 PYCINEMA = { 'VERSION' : '2.1.0'}
 
-# layout
-vf0 = pycinema.theater.Theater.instance.centralWidget()
-vf0.setHorizontalOrientation()
-vf0.insertView( 0, pycinema.theater.views.NodeEditorView() )
-vf1 = vf0.insertFrame(1)
-vf1.setVerticalOrientation()
-ImageView_1 = vf1.insertView( 0, pycinema.theater.views.ImageView() )
-ImageView_2 = vf1.insertView( 1, pycinema.theater.views.ImageView() )
-vf1.setSizes([366, 476])
-vf0.setSizes([989, 989])
-
 # filters
 CinemaDatabaseReader_0 = pycinema.filters.CinemaDatabaseReader()
 ImageReader_0 = pycinema.filters.ImageReader()
-ImagesToTable_0 = pycinema.filters.ImagesToTable()
 Python_0 = pycinema.filters.Python()
+ImageView_0 = pycinema.filters.ImageView()
+ImageView_1 = pycinema.filters.ImageView()
 
 # properties
 CinemaDatabaseReader_0.inputs.path.set("data/sphere.cdb", False)
@@ -29,11 +19,33 @@ CinemaDatabaseReader_0.inputs.file_column.set("FILE", False)
 ImageReader_0.inputs.table.set(CinemaDatabaseReader_0.outputs.table, False)
 ImageReader_0.inputs.file_column.set("FILE", False)
 ImageReader_0.inputs.cache.set(True, False)
-ImageView_1.inputs.images.set(ImageReader_0.outputs.images, False)
-ImagesToTable_0.inputs.images.set(ImageReader_0.outputs.images, False)
-Python_0.inputs.inputs.set(ImagesToTable_0.outputs.table, False)
+Python_0.inputs.inputs.set(CinemaDatabaseReader_0.outputs.table, False)
 Python_0.inputs.code.set("examples/pythonfilter/ImageScatterplot.py", False)
-ImageView_2.inputs.images.set(Python_0.outputs.outputs, False)
+ImageView_0.inputs.images.set(Python_0.outputs.outputs, False)
+ImageView_0.inputs.selection.set([], False)
+ImageView_1.inputs.images.set(ImageReader_0.outputs.images, False)
+ImageView_1.inputs.selection.set([], False)
+
+# layout
+tabFrame1 = pycinema.theater.TabFrame()
+splitFrame1 = pycinema.theater.SplitFrame()
+splitFrame1.setHorizontalOrientation()
+view1 = pycinema.theater.views.NodeEditorView()
+splitFrame1.insertView( 0, view1 )
+splitFrame1.setSizes([1018])
+tabFrame1.insertTab(0, splitFrame1)
+tabFrame1.setTabText(0, 'Layout 1')
+splitFrame2 = pycinema.theater.SplitFrame()
+splitFrame2.setHorizontalOrientation()
+view7 = pycinema.theater.views.FilterView( ImageView_1 )
+splitFrame2.insertView( 0, view7 )
+view9 = pycinema.theater.views.FilterView( ImageView_0 )
+splitFrame2.insertView( 1, view9 )
+splitFrame2.setSizes([506, 505])
+tabFrame1.insertTab(1, splitFrame2)
+tabFrame1.setTabText(1, 'Layout 2')
+tabFrame1.setCurrentIndex(1)
+pycinema.theater.Theater.instance.setCentralWidget(tabFrame1)
 
 # execute pipeline
 CinemaDatabaseReader_0.update()
