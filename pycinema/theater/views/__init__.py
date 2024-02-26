315cbbe8f47b86e77f8814f97159c5d9b5844441
David H. Rogers <dhr@lanl.gov>
2024-02-26 14:13:33 -0700

    removing obsolete classes

--- pycinema/theater/views/__init__.py
+++ pycinema/theater/views/__init__.py
@@ -1,7 +1,4 @@
 from .FilterView import FilterView
 from .SelectionView import SelectionView
 from .NodeEditorView import NodeEditorView, QtNodeEditorView
-from .PlotBarView import PlotBarView
-from .PlotLineView import PlotLineView
-from .PlotScatterView import PlotScatterView
 
