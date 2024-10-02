1be4e980b8e6d14620d3cf6f8d7daaacc5fd5218
David H. Rogers <dhr@lanl.gov>
2024-10-02 10:52:58 -0600

    update

--- pycinema/scripts/browse.py
+++ pycinema/scripts/browse.py
@@ -4,20 +4,14 @@ import pycinema.theater
 import pycinema.theater.views
 
 # pycinema settings
-PYCINEMA = { 'VERSION' : '2.1.0'}
-
-# this application settings
-BROWSE = { 'VERSION' : '1.0'}
-
-# reporting
-print("browse v" + BROWSE["VERSION"])
+PYCINEMA = { 'VERSION' : '3.1.0'}
 
 # filters
 CinemaDatabaseReader_0 = pycinema.filters.CinemaDatabaseReader()
 TableQuery_0 = pycinema.filters.TableQuery()
 ImageReader_0 = pycinema.filters.ImageReader()
 TableView_0 = pycinema.filters.TableView()
-ImageView_1 = pycinema.filters.ImageView()
+ImageView_0 = pycinema.filters.ImageView()
 
 # properties
 CinemaDatabaseReader_0.inputs.path.set(PYCINEMA_ARG_0, False)
@@ -28,30 +22,30 @@ ImageReader_0.inputs.table.set(TableQuery_0.outputs.table, False)
 ImageReader_0.inputs.file_column.set("FILE", False)
 ImageReader_0.inputs.cache.set(True, False)
 TableView_0.inputs.table.set(TableQuery_0.outputs.table, False)
-TableView_0.inputs.selection.set([], False)
-ImageView_1.inputs.images.set(ImageReader_0.outputs.images, False)
-ImageView_1.inputs.selection.set([], False)
+TableView_0.inputs.selection.set(ImageView_0.inputs.selection, False)
+ImageView_0.inputs.images.set(ImageReader_0.outputs.images, False)
+ImageView_0.inputs.selection.set([11], False)
 
 # layout
-tabFrame0 = pycinema.theater.TabFrame()
-splitFrame0 = pycinema.theater.SplitFrame()
-splitFrame0.setHorizontalOrientation()
-view0 = pycinema.theater.views.NodeEditorView()
-splitFrame0.insertView( 0, view0 )
-splitFrame0.setSizes([1018])
-tabFrame0.insertTab(0, splitFrame0)
-tabFrame0.setTabText(0, 'Layout 1')
+tabFrame1 = pycinema.theater.TabFrame()
 splitFrame1 = pycinema.theater.SplitFrame()
 splitFrame1.setHorizontalOrientation()
+view1 = pycinema.theater.views.NodeEditorView()
+splitFrame1.insertView( 0, view1 )
+splitFrame1.setSizes([1633])
+tabFrame1.insertTab(0, splitFrame1)
+tabFrame1.setTabText(0, 'Layout 1')
+splitFrame2 = pycinema.theater.SplitFrame()
+splitFrame2.setHorizontalOrientation()
 view2 = pycinema.theater.views.FilterView( TableView_0 )
-splitFrame1.insertView( 0, view2 )
-view8 = pycinema.theater.views.FilterView( ImageView_1 )
-splitFrame1.insertView( 1, view8 )
-splitFrame1.setSizes([570, 568])
-tabFrame0.insertTab(1, splitFrame1)
-tabFrame0.setTabText(1, 'Layout 2')
-tabFrame0.setCurrentIndex(1)
-pycinema.theater.Theater.instance.setCentralWidget(tabFrame0)
+splitFrame2.insertView( 0, view2 )
+view3 = pycinema.theater.views.FilterView( ImageView_0 )
+splitFrame2.insertView( 1, view3 )
+splitFrame2.setSizes([814, 812])
+tabFrame1.insertTab(1, splitFrame2)
+tabFrame1.setTabText(1, 'Layout 2')
+tabFrame1.setCurrentIndex(1)
+pycinema.theater.Theater.instance.setCentralWidget(tabFrame1)
 
 # execute pipeline
 CinemaDatabaseReader_0.update()
