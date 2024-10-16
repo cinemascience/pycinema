b67829b61e1740436eefdc7fc7f6ba36b77fd317
David H. Rogers <dhr@lanl.gov>
2024-10-16 16:57:31 -0600

    adding new command

--- pycinema/scripts/browse.py
+++ pycinema/scripts/browse.py
@@ -24,7 +24,7 @@ ImageReader_0.inputs.cache.set(True, False)
 TableView_0.inputs.table.set(TableQuery_0.outputs.table, False)
 TableView_0.inputs.selection.set(ImageView_0.inputs.selection, False)
 ImageView_0.inputs.images.set(ImageReader_0.outputs.images, False)
-ImageView_0.inputs.selection.set([11], False)
+ImageView_0.inputs.selection.set([], False)
 
 # layout
 tabFrame1 = pycinema.theater.TabFrame()
